26bf94a81e1f13570b4fe701c76ca01196a90d0f
abrahamMartinez <bmtzs@me.com>
2023-08-08 14:49:59 +0200

    modified

--- app.js
+++ app.js
@@ -11,6 +11,69 @@ console.log(timer.shortBreak);
 console.log(timer.longBreak);
 console.log(timer.sets);
 
+const modeButtons = document.querySelector
+
+function switchMode(mode) {
+    timer.mode = mode;
+    timer.remainingTime = {
+        total: timer[mode] * 60,
+        minutes: timer[mode],
+        seconds: 0,
+    }
+}
+
+function updateClock(){
+    const {remainingTime } = timer;
+    const minutes = `${remainingTime.minutes}`.padStart(2, '0');
+    const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+
+    const min = document.querySelector('#timer_minutes');
+    const sec = document.querySelector('#timer_seconds');
+
+    min.textContent = minutes;
+    sec.textContent = seconds;
+}
+
+let interval;
+
+const startBtn = document.querySelector('#startBtn')
+startBtn.addEventListener('click', () => {
+    // const { action } = start.Btn.dataset;
+    // if (action === 'start') {
+        startTimer();
+    // }
+});
+
+function startTimer() {
+    let { total } = timer.remainingTime;
+    const endTime = Date.parse(new Date ()) + total * 1000;
+
+    interval = setInterval(function () {
+        timer.remainingTime = getRemainingTime (endTime);
+        updateClock();
+
+        total = timer.remainingTime.total;
+        if (total <= 0) {
+            clearInterval(interval);
+        }
+    }, 1000);
+}
+
+function getRemainingTime(endTime) {
+    const currentTime = Date.parse(new Date());
+    const difference = endTime - currentTime;
+
+    const total = Number.parseInt(difference / 100, 10);
+    const minutes = Number.parseInt((total / 60) % 60, 10);
+    const seconds = Number.parseInt(total % 60, 10);
+
+    return {
+        total,
+        minutes,
+        seconds
+    }
+}
+
 // // const blocks
 // // const keepWorkingTime = 5 mins
 
